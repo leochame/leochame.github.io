4a94ab4fccc060caa43409b7a58a130ca1b21140
leochame <leocham.cn@gmail.com>
2025-11-21 09:27:49 +0800

    update

--- js/articles.js
+++ js/articles.js
@@ -97,30 +97,6 @@ const articlesData = [
         isResearch: false
     },
     {
-        featured: false,
-        id: 17,
-        title: "从NTP时钟同步优化角度出发 - 大学知识不再是空中楼阁（一）",
-        summary: "本系列基于数学，操作系统，计算机网络及网络编程角度出发去探寻 NTP 时钟同步的优化。将大学所学应用在这个优化上。",
-        link: "https://juejin.cn/post/7480441580059852835",
-        platform: "掘金",
-        date: "2025-03-12",
-        mainCategory: "计算机基础",
-        subCategory: "NTP",
-        isResearch: true // 研究札记
-    },
-    {
-        featured: false,
-        id: 16,
-        title: "从NTP时钟同步优化角度出发 - 大学知识不再是空中楼阁（二）",
-        summary: "文章主要从网络角度探讨NTP时钟同步的优化，先指出原始方法性能差的原因，包括客户端实例重复创建等，然后介绍使用NIO的优化方式，如连接复用、非阻塞实现、使用Selector等，还对比了原方案和NIO优化方案，最后讨论了为何使用NIO而非Netty优化及Linux下的Epoll Bug和Netty的解决办法，并在附录中介绍了ByteBuffer的相关知识。",
-        link: "https://juejin.cn/post/7480777354932109349",
-        platform: "掘金",
-        date: "2025-03-12",
-        mainCategory: "计算机基础",
-        subCategory: "NTP",
-        isResearch: true
-    },
-    {
         featured: true,
         id: 15,
         title: "Tracing the thoughts of a large language model",
